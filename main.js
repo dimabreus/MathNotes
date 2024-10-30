87137ce322ad6979034a3da973860000d31f6442
dimabreus <dmitriybreus5@gmail.com>
2024-10-31 00:01:07 +0300

    Added system linear equations support

--- main.js
+++ main.js
@@ -9,6 +9,112 @@ function resetVariables() {
   }
 }
 
+function solve(equations) {
+  const names = {};
+  const m = [];
+  const b = [];
+  const result = new Map();
+  let size = 0;
+
+  const gcd = (a, b) => (b ? gcd(b, a % b) : a);
+
+  const simplify = (i) => {
+    let g = 0;
+    const currentRow = m[i];
+    for (const x of currentRow) {
+      g = x ? (g ? gcd(g, Math.abs(x)) : x) : g;
+    }
+    if (g && g > 1) {
+      for (let j = 0; j <= size; ++j) {
+        currentRow[j] /= g;
+      }
+    }
+  };
+
+  for (const eq of equations) {
+    const line = [];
+    let r = 0;
+    const sides = eq.split('=');
+
+    for (let i = 0; i < 2; i++) {
+      const side = sides[i] || '0';
+      side.match(/[+\-]?[a-z0-9]+/gi).forEach((part) => {
+        const coef = (part[0] === '-' ? -1 : 1) * (1 - i * 2) * (part.match(/\d+/g) || [1])[0];
+        const name = (part.match(/[a-z]+/g) || [])[0];
+
+        if (name) {
+          if (names[name] === undefined) names[name] = size++;
+          line[names[name]] = (line[names[name]] || 0) + coef;
+        } else {
+          r -= coef;
+        }
+      });
+    }
+
+    m.push(line);
+    b.push(r);
+  }
+
+  for (const s of m) {
+    for (let j = 0; j < size; ++j) {
+      if (!s[j]) {
+        s[j] = 0;
+      }
+    }
+    s[size] = b[m.indexOf(s)];
+  }
+
+  simplify(0);
+  for (let i = 0; i < m.length; ++i) {
+    const currentRow = m[i];
+    const v = currentRow.findIndex((x) => x);
+    if (v === -1) continue;
+
+    for (let j = i + 1; j < m.length; ++j) {
+      if (m[j][v]) {
+        const g = gcd(Math.abs(currentRow[v]), Math.abs(m[j][v]));
+        const a = currentRow[v] / g;
+        const b = m[j][v] / g;
+        for (let k = 0; k <= size; ++k) {
+          m[j][k] = m[j][k] * a - currentRow[k] * b;
+        }
+        simplify(j);
+      }
+    }
+  }
+
+  for (let i = m.length - 1; i >= 0; --i) {
+    if (!m[i].some((x) => x)) {
+      m.splice(i, 1);
+    } else if (!m[i].some((x, j) => x && j !== size)) {
+      return null;
+    }
+  }
+
+  if (m.length !== size) {
+    return null;
+  }
+
+  for (let i = m.length - 1; i >= 0; --i) {
+    let sum = 0;
+    let nextName;
+
+    for (const name in names) {
+      if (m[i][names[name]]) {
+        if (result.has(name)) {
+          sum += result.get(name) * m[i][names[name]];
+        } else {
+          nextName = name;
+        }
+      }
+    }
+
+    result.set(nextName, (m[i][size] - sum) / m[i][names[nextName]]);
+  }
+
+  return result;
+}
+
 function preprocessExpression(expression) {
   expression = expression
     .replaceAll('^', '**')
@@ -57,6 +163,16 @@ function handleChange(lines) {
   const newLines = [];
 
   resetVariables();
+
+  try {
+    const newVariables = solve(lines.filter(x => x && x.includes('=') && x.split('=').every(Boolean)).map(line => line.trim().replaceAll(' ', '')));
+
+    newVariables.entries().forEach(([key, value]) => {
+      if (!Object.keys(variables).includes(key)) {
+        variables[key] = value;
+      }
+    });
+  } catch { }
 
   lines.forEach((line) => {
     const parse = tryParse(line);
